fd4f74f9cfd83c41196212aca49350071af0753b
zxdr4gon <zx.mahdi.499@gmail.com>
2025-07-06 17:35:51 -0500

    Update number_guessing.py

--- number_guessing.py
+++ number_guessing.py
@@ -1,1 +1,14 @@
 # Number guessing game
+
+import random
+import sleep
+import sys
+import time
+
+def typewriter(text, delay=0.03):
+# Prints the text in a typewriter effect, +/- delay to make faster/slower
+  for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()  # Newline at the end
